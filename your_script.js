5429383f1a0e9b9babfd5d15452e75734b8f4b5e
MalcolmPete <58274443+MalcolmPete@users.noreply.github.com>
2023-07-29 23:42:37 -0400

    Update your_script.js
    change to pictures

--- your_script.js
+++ your_script.js
@@ -79,7 +79,7 @@ function displayItemInHTML(playersData) {
       detailsDiv.className = 'details';
   
       const image = document.createElement('img');
-      image.src = `/PlayerPictures/${player.name}.png`;
+      image.src = 'PlayerPictures/${player.name}.png';
   
       const playerNameSpan = document.createElement('span');
       playerNameSpan.textContent = player.name;
