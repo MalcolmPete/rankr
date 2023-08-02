071b8b62b125cc38e6ebac998b35297de61f621a
MalcolmPete <58274443+MalcolmPete@users.noreply.github.com>
2023-08-01 20:39:41 -0400

    Update your_script.js
    Patch for excel export function

--- your_script.js
+++ your_script.js
@@ -163,17 +163,19 @@ document.addEventListener('DOMContentLoaded', () => {
 function exportDataToExcel(data) {
     const XLSX = require('xlsx');
     const { saveAs } = require('file-saver');
-  
+
     // Create a new workbook and worksheet
     const workbook = XLSX.utils.book_new();
-    const worksheet = XLSX.utils.aoa_to_sheet([['Player Name', 'Position', 'Bye'], ...data]);
-  
+    const worksheet = XLSX.utils.json_to_sheet(data, {
+      header: ['name', 'position', 'bye'], // Set the headers for the columns
+    });
+
     // Add the worksheet to the workbook
     XLSX.utils.book_append_sheet(workbook, worksheet, 'Player Data');
-  
+
     // Convert the workbook to an Excel file
     const excelFile = XLSX.write(workbook, { type: 'array', bookType: 'xlsx' });
-  
+
     // Save the Excel file
     const blob = new Blob([excelFile], { type: 'application/octet-stream' });
     saveAs(blob, 'player_data.xlsx');
