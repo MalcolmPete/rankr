a2045aab3da04fb655592db042c1729d28c1fbfd
MalcolmPete <58274443+MalcolmPete@users.noreply.github.com>
2023-08-08 20:01:29 -0400

    Update your_script.js
    Added excel input support, positional rankings and bug fixes

--- your_script.js
+++ your_script.js
@@ -2,6 +2,9 @@ const sleeperUrl = "https://api.sleeper.app/v1/league/968222929374822400";
 const backendURL = 'http://localhost:3000';
 const localStorageKey = 'playerOrder'; // Define a key for local storage
 let extractedData = []; // Declare extractedData outside the function scope
+let positionFilter = null;
+let overallRankCounter = 0;
+let positionRanks = {};
 
 async function getLeagueInfo() {
   // Fetches the URL
@@ -38,86 +41,133 @@ async function getADPInfo(scoringName, numOfTeams, season) {
 
   const extractedData = ADPdata.players.map((player, index) => {
     const { name, position, bye, team, adp_formatted } = player;
-    return { index: index + 1, name, position, bye, team, adp_formatted };
-  });
+    return {
+      index: index + 1,
+      name,
+      position,
+      bye,
+      team,
+      adp_formatted,
+      totalRank: index + 1,
+      positionRank: `${position}${positionRanks[position]}`,
+    };
+  }); 
 
   return extractedData; // Return the extracted data
 }
 
 function displayItemInHTML(playersData) {
-    const listContainer = document.getElementById('sortableListContainer');
-  
-    // Create a header element for the "rankr" title
+  const listContainer = document.getElementById('sortableListContainer');
+
+  // Remove existing players before updating the list
+  listContainer.innerHTML = '';
+
+  // Create a header element for the "rankr" title if it doesn't exist
+  if (!document.querySelector('.rankr-title')) {
     const header = document.createElement('h1');
     header.textContent = 'rankr';
-    header.className = 'rankr-title'; // Add a class for styling the title
-    listContainer.insertBefore(header, listContainer.firstChild);
+    header.className = 'rankr-title';
+    listContainer.appendChild(header);
+  }
 
+  // Create a header element for the "alpha build" title if it doesn't exist
+  if (!document.querySelector('.alpha-header')) {
     const alphaHeader = document.createElement('h2');
     alphaHeader.textContent = 'alpha build';
-    alphaHeader.className = 'alpha-header'; // Add a class for styling the alpha header
-    listContainer.insertBefore(alphaHeader, header.nextSibling);
-  
-    // Retrieve the saved player order from local storage
-    const savedPlayerOrder = JSON.parse(localStorage.getItem(localStorageKey));
-  
-    // If the saved order exists, use it to sort the playersData array
-    if (Array.isArray(savedPlayerOrder) && savedPlayerOrder.length === playersData.length) {
-      playersData.sort((a, b) => {
-        const aIndex = savedPlayerOrder.indexOf(a.name);
-        const bIndex = savedPlayerOrder.indexOf(b.name);
-        return aIndex - bIndex;
-      });
-    }
-  
-    for (let i = 0; i < playersData.length; i++) {
-      const player = playersData[i];
-  
-      const listItem = document.createElement('li');
-      listItem.className = 'item';
-      listItem.draggable = true;
-  
-      const containerDiv = document.createElement('div'); // Container for grouping elements
-      containerDiv.className = 'player-container'; // Add a CSS class for styling if needed
-  
-      const detailsDiv = document.createElement('div');
-      detailsDiv.className = 'details';
-  
-      const image = document.createElement('img');
-      image.src = `PlayerPictures/${player.name.replace(/\s/g, '_')}.png`;
-  
-      const playerNameSpan = document.createElement('span');
-      playerNameSpan.textContent = player.name;
-      playerNameSpan.className = 'player-name'; // Add a class for player name span
-  
-      const byeSpan = document.createElement('span');
-      byeSpan.textContent = `Bye: ${player.bye}`;
-  
-      const positionSpan = document.createElement('span');
-      positionSpan.textContent = `Position: ${player.position}`;
-  
-      const teamSpan = document.createElement('span');
-      teamSpan.textContent = `Team: ${player.team}`;
+    alphaHeader.className = 'alpha-header';
+    listContainer.appendChild(alphaHeader);
+  }
 
-      const adpFormattedSpan = document.createElement('span');
-      adpFormattedSpan.textContent = `ADP: ${player.adp_formatted}`;
+  // Reset counters and position ranks
+  overallRankCounter = 0;
+  positionRanks = {};
 
-      detailsDiv.appendChild(image);
-      detailsDiv.appendChild(playerNameSpan);
-      detailsDiv.appendChild(byeSpan);
-      detailsDiv.appendChild(positionSpan);
-      detailsDiv.appendChild(teamSpan); // Add team information to details
-      detailsDiv.appendChild(adpFormattedSpan); // Add adp_formatted information to details
-  
-      const draggableDotsIcon = document.createElement('i');
-      draggableDotsIcon.className = 'uil uil-draggabledots';
-  
-      containerDiv.appendChild(detailsDiv); // Add the details to the container
-      containerDiv.appendChild(draggableDotsIcon);
-  
-      listItem.appendChild(containerDiv); // Add the container to the list item
-      listContainer.appendChild(listItem);
+  // Retrieve the saved player order from local storage
+  const savedPlayerOrder = JSON.parse(localStorage.getItem(localStorageKey));
+
+  // If the saved order exists, use it to sort the playersData array
+  if (Array.isArray(savedPlayerOrder) && savedPlayerOrder.length === playersData.length) {
+    playersData.sort((a, b) => {
+      const aIndex = savedPlayerOrder.indexOf(a.name);
+      const bIndex = savedPlayerOrder.indexOf(b.name);
+      return aIndex - bIndex;
+    });
+  }
+
+  for (let i = 0; i < playersData.length; i++) {
+    const player = playersData[i];
+
+    // Check the position filter and skip players if the filter is set and doesn't match
+    if (positionFilter && player.position !== positionFilter) {
+      continue;
     }
+
+    overallRankCounter++;
+
+    const listItem = document.createElement('li');
+    listItem.className = 'item';
+    listItem.draggable = true;
+
+    const containerDiv = document.createElement('div');
+    containerDiv.className = `player-container ${player.position.toLowerCase()}`; // Add a class for styling and position-based color
+
+    const detailsDiv = document.createElement('div');
+    detailsDiv.className = 'details';
+
+    // Create and append the overall rank
+    const overallRankSpan = document.createElement('span');
+    overallRankSpan.textContent = overallRankCounter;
+    overallRankSpan.className = 'overall-rank';
+    detailsDiv.appendChild(overallRankSpan);
+
+    // Create and append the position rank
+    if (!positionRanks[player.position]) {
+      positionRanks[player.position] = 1;
+    } else {
+      positionRanks[player.position]++;
+    }
+    const positionRankSpan = document.createElement('span');
+    positionRankSpan.textContent = `${player.position}${positionRanks[player.position]}`;
+    positionRankSpan.className = 'position-rank';
+    positionRankSpan.setAttribute('data-position', player.position); // Set the data-position attribute
+    positionRankSpan.setAttribute('data-rank', positionRanks[player.position]); // Set the data-rank attribute
+    detailsDiv.appendChild(positionRankSpan);
+
+    const image = document.createElement('img');
+    image.src = `PlayerPictures/${player.name.replace(/\s/g, '_')}.png`;
+
+    const playerNameSpan = document.createElement('span');
+    playerNameSpan.textContent = player.name;
+    playerNameSpan.className = 'player-name'; // Add a class for player name span
+
+    const byeSpan = document.createElement('span');
+    byeSpan.textContent = `Bye: ${player.bye}`;
+
+    const positionSpan = document.createElement('span');
+    positionSpan.textContent = `Position: ${player.position}`;
+
+    const teamSpan = document.createElement('span');
+    teamSpan.textContent = `Team: ${player.team}`;
+
+    const adpFormattedSpan = document.createElement('span');
+    adpFormattedSpan.textContent = `ADP: ${player.adp_formatted}`;
+
+    detailsDiv.appendChild(image);
+    detailsDiv.appendChild(playerNameSpan);
+    detailsDiv.appendChild(byeSpan);
+    detailsDiv.appendChild(positionSpan);
+    detailsDiv.appendChild(teamSpan); // Add team information to details
+    detailsDiv.appendChild(adpFormattedSpan); // Add adp_formatted information to details
+
+    const draggableDotsIcon = document.createElement('i');
+    draggableDotsIcon.className = 'uil uil-draggabledots';
+
+    containerDiv.appendChild(detailsDiv); // Add the details to the container
+    containerDiv.appendChild(draggableDotsIcon);
+
+    listItem.appendChild(containerDiv);
+    listContainer.appendChild(listItem);
+  }
 }
 
 function handleReorder(event) {
@@ -134,6 +184,67 @@ function handleReorder(event) {
     return playerName;
   });
   localStorage.setItem(localStorageKey, JSON.stringify(playerOrder));
+
+  updateRanks(playerElements);
+}
+
+function updateRanks(playerElements) {
+  overallRankCounter = 0;
+  const updatedPositionRanks = {}; // Create a new object for updated position ranks
+
+  playerElements.forEach((playerElement, index) => {
+    const playerName = playerElement.querySelector('.player-name').textContent;
+    const playerPosition = playerElement.querySelector('.position-rank').getAttribute('data-position'); // Get player position
+
+    if (!updatedPositionRanks[playerPosition]) {
+      updatedPositionRanks[playerPosition] = 1;
+    } else {
+      updatedPositionRanks[playerPosition]++;
+    }
+
+    const positionRank = updatedPositionRanks[playerPosition]; // Get position rank from updatedPositionRanks
+
+    // Update the player object's positionRank
+    const playerIndex = extractedData.findIndex(player => player.name === playerName);
+    extractedData[playerIndex].positionRank = `${playerPosition}${positionRank}`;
+
+    overallRankCounter++;
+
+    const overallRankSpan = playerElement.querySelector('.overall-rank');
+    const positionRankSpan = playerElement.querySelector('.position-rank');
+
+    overallRankSpan.textContent = index + 1;
+    positionRankSpan.textContent = `${playerPosition}${positionRank}`;
+    positionRankSpan.setAttribute('data-rank', positionRank); // Update the data-rank attribute
+  });
+
+  positionRanks = updatedPositionRanks; // Update positionRanks with the new values
+}
+
+function updatePositionRanks(playerElements) {
+  positionRanks = {}; // Reset positionRanks
+
+  playerElements.forEach((playerElement, index) => {
+    const playerPosition = playerElement.querySelector('.position-rank').getAttribute('data-position');
+
+    if (!positionRanks[playerPosition]) {
+      positionRanks[playerPosition] = 1;
+    } else {
+      positionRanks[playerPosition]++;
+    }
+
+    const positionRank = positionRanks[playerPosition];
+
+    const playerName = playerElement.querySelector('.player-name').textContent;
+    const playerIndex = extractedData.findIndex(player => player.name === playerName);
+
+    // Update the player object's positionRank
+    extractedData[playerIndex].positionRank = `${playerPosition}${positionRank}`;
+
+    const positionRankSpan = playerElement.querySelector('.position-rank');
+    positionRankSpan.textContent = `${playerPosition}${positionRank}`;
+    positionRankSpan.setAttribute('data-rank', positionRank); // Update the data-rank attribute
+  });
 }
 
 // Event listeners for drag-and-drop functionality
@@ -165,26 +276,54 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Event listener for when a player line container is dropped
-  listContainer.addEventListener('drop', (event) => {
+  listContainer.addEventListener('drop', async (event) => {
     event.preventDefault();
-
+  
     // Update the order in the local storage after the drop is complete
     handleReorder(event);
+  
+    // Wait for a brief moment before updating the ranks
+    await delay(100);
+  
+    // Update the position ranks
+    updatePositionRanks(listContainer.querySelectorAll('.player-container'));
+  
+    // Update the displayed players
+    displayItemInHTML(extractedData);
   });
 
   // Event listener for when a player line container is dragged over
   listContainer.addEventListener('dragend', handleReorder);
+
+  // Event listener for position buttons
+  const positionButtons = document.querySelectorAll('.position-button');
+  positionButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        positionFilter = button.getAttribute('data-position');
+
+        // For the "All" button, set the position filter to null
+        if (positionFilter === "All") {
+            positionFilter = null;
+        }
+
+        displayItemInHTML(extractedData); // Update the displayed players
+    });
+  });
 });
 
 function exportDataToExcel(data) {
   const XLSX = window.XLSX; // Access XLSX library from the global object
 
-  // Create the worksheet from the data
-  const worksheet = XLSX.utils.json_to_sheet(data);
-
-  // Create a new workbook and add the worksheet to it
+  // Create a new workbook
   const workbook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(workbook, worksheet, 'Player Data');
+
+  // Create separate sheets for each position
+  const positions = ['All', 'QB', 'RB', 'WR', 'TE'];
+  positions.forEach(position => {
+    const filteredData = position === 'All' ? data : data.filter(player => player.position === position);
+    const worksheet = XLSX.utils.json_to_sheet(filteredData);
+    XLSX.utils.book_append_sheet(workbook, worksheet, position);
+  });
 
   // Convert the workbook to an XLSX file
   const excelFile = XLSX.write(workbook, { type: 'array', bookType: 'xlsx' });
@@ -215,8 +354,59 @@ function exportDataToExcel(data) {
 
 // Event listener for the "Export to Excel" button click
 const exportButton = document.getElementById('exportButton');
-exportButton.addEventListener('click', () => {
+exportButton.addEventListener('click', async () => {
+    // Add a delay of 1 second (1000 milliseconds) before exporting
+    await delay(1000);
     exportDataToExcel(extractedData);
 });
+
+// Helper function to introduce a delay using Promises
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Event listener for the "Upload Excel" button click
+const uploadExcelButton = document.getElementById('uploadExcelButton');
+uploadExcelButton.addEventListener('click', () => {
+  const uploadExcelInput = document.getElementById('uploadExcelInput');
+  uploadExcelInput.click(); // Trigger the input click
+});
+
+// Event listener for file selection in the input
+const uploadExcelInput = document.getElementById('uploadExcelInput');
+uploadExcelInput.addEventListener('change', async (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const data = await readExcelFile(file);
+    integrateExcelData(data);
+  }
+});
+
+// Helper function to read Excel files
+async function readExcelFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const data = event.target.result;
+      const workbook = XLSX.read(data, { type: 'array' });
+      const firstSheetName = workbook.SheetNames[0];
+      const excelData = XLSX.utils.sheet_to_json(workbook.Sheets[firstSheetName]);
+      resolve(excelData);
+    };
+    reader.onerror = (error) => {
+      reject(error);
+    };
+    reader.readAsArrayBuffer(file);
+  });
+}
+
+// Helper function to integrate Excel data
+function integrateExcelData(excelData) {
+  // Update the player order and ranks based on the Excel data
+  const playerOrder = excelData.map(player => player.name);
+  localStorage.setItem(localStorageKey, JSON.stringify(playerOrder));
+  updatePositionRanks(document.querySelectorAll('.player-container'));
+  displayItemInHTML(extractedData);
+}
   
 getLeagueInfo();
